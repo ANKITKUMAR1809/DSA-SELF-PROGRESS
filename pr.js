db9f8128751b918740e7ef599139fb62137e549d
Ankitkumar1809 <skwedak143@gmail.com>
2025-12-25 21:13:48 +0530

    practicing the buy today and sell tommorow

--- pr.js
+++ pr.js
@@ -1,30 +1,17 @@
-const nums1 = []
-const nums2 = [2,3]
-var findMedianSortedArrays = function (nums1, nums2) {
-    let median = 0;
-    let nums3 = [];
-    let p1 = 0, p2 = 0;
-    let m = nums1.length;
-    let n = nums2.length
-    for (let i = 0; i < m + n; i++) {
-        if (p2 >= n || (p1 < m && nums1[p1] < nums2[p2])) {
-            nums3[i] = nums1[p1]
-            p1++
-        }
-        else {
-            nums3[i] = nums2[p2]
-            p2++
-        }
-    }
-    
-    if (nums3.length % 2 == 0) {
-      let mid=nums3.length/2;
-      median = (nums3[mid] + nums3[mid - 1]) / 2
-    } else {
-      console.log("comes odd")
-        median = nums3[Math.trunc(nums3.length/2)]
-    }
-    return median;
-};
+const arr = [ 7,1, 5, 3, 6, 4]
 
-console.log(findMedianSortedArrays(nums1,nums2))
+const btst = (arr) =>{
+    let min = arr[0];
+    let max=0;
+    for( let i =1; i<arr.length; i++){
+        if(arr[i]-min>max){
+            max=arr[i]-min;
+        }
+        if(arr[i]<min){
+            min=arr[i]
+        }
+    }
+    return max;
+}
+
+console.log(btst(arr))
