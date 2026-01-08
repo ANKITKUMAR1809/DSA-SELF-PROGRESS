74ba3c62c875bc31e8714e3c56ea0f18ec6463a6
Ankitkumar1809 <skwedak143@gmail.com>
2026-01-08 22:17:54 +0530

    reverse linked list done

---
+++ Linked List/LinkedList.js
@@ -0,0 +1,5 @@
+console.log(0/10+1)
+console.log(1/10+2)
+console.log(2.1/10+2)
+console.log(2.21/10+1)
+console.log(parseInt(1.221*(10**3)))
---
+++ Linked List/PallindromeLinkedList.js
@@ -0,0 +1,30 @@
+// 1 -> 2 -> 3 -> 2 -> 1 -> null
+function isPallindrome (head){
+    // finding mid by fast and slow method
+    let fast=slow=head;
+    while(fast && fast.next){
+        slow=slow.next;
+        fast=fast.next.next;
+    }
+    // now we find the midd as slow 
+    // now reverse the linkedlist from mid to null;
+    let curr=slow;
+    let prev=null;
+    while(curr){
+        let temp=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=temp;
+    }
+    // after this we got the reverse list from mid to null that is prev;
+    let second = prev;
+    let first=head;
+    while(second){
+        if(first.val!=second.val){
+            return false;
+        }
+        first=first.next;
+        second=second.next;
+    }
+    return true;
+}
