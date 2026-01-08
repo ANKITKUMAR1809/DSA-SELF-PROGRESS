7849cb4413a79e030da520a3b58e3a1030e7c326
Ankitkumar1809 <skwedak143@gmail.com>
2026-01-08 19:44:25 +0530

    done merge sort linked List

---
+++ Practice/MaxConsecutive.js
@@ -0,0 +1,17 @@
+let arr = [ 1,1,0,1,1,1,0,1]
+// binary array
+// output - max consecutive of 1.
+
+let maxCount = 0;
+let currentCount = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] === 1) {
+    currentCount++;
+    maxCount = Math.max(maxCount, currentCount);
+  } else {
+    currentCount = 0;
+  }
+}
+
+console.log(maxCount);
