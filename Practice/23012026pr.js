60cf7baccafd433db10baaeb7c3498594da4c790
ANKITKUMAR1809 <skwedsak143@gmail.com>
2026-04-06 22:14:53 +0530

    add initial implementation for ValidPallindrome and array manipulation

---
+++ Practice/23012026pr.js
@@ -0,0 +1,12 @@
+let arr = [0, 1, 0, 3, 12];
+
+let x =0;
+for ( let i=0; i<arr.length;i++){
+    if(arr[i]!= 0){
+        let temp = arr[i]
+        arr[i]=arr[x]
+        arr[x]=temp;
+        x++;
+    }
+}
+console.log(arr)
